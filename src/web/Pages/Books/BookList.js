0f806991ed2f402389f84b095b0ea37842413d32
pnbgdn <pnbgdn@protonmail.com>
2022-06-01 21:38:34 +0300

    added basic BookList

--- src/web/Pages/Books/BookList.js
+++ src/web/Pages/Books/BookList.js
@@ -1,15 +1,50 @@
 import { Page, PageContent } from "web/Components/Common";
+import { ResourceCollectionWithFetch, ResourceListRow } from "web/Components/ResourceCollection";
 
-export default function BookList({})
+import { Middleware } from "web/Middleware";
+import { useSmartCall } from "web/Hooks";
+import { useCallback, useEffect, useState } from "react";
+import BookListRow from "./BookListRow";
+
+export default function BookList()
 {
-    return (
-        <Page>
-            <PageContent
-                title="Books"
-                description="A book is a medium for recording information in the form of writing or images, typically composed of many pages bound together and protected by a cover."
-            >
-                Book List
-            </PageContent>
-        </Page>
-    );
+	const [bookList, setBookList] = useState([]);
+
+	const fetchInternal = useCallback(async () => {
+		const response = await Middleware.fetchBooks();
+		setBookList(response);
+	}, []);
+
+	const [fetchData, isLoading, error] = useSmartCall(fetchInternal);
+
+	useEffect(() => { fetchData(); }, []);
+
+	console.log({ bookList });
+
+	return (
+		<Page>
+			<PageContent
+				title="Books"
+				description="A book is a medium for recording information in the form of writing or images, typically composed of many pages bound together and protected by a cover."
+			>
+				<ResourceCollectionWithFetch
+					fetchData = {fetchData}
+					isLoading = {isLoading}
+
+					data = {bookList}
+
+					dataFetchError = {error}
+					rowsPerPage = {2}
+					renderRow={(context) => (
+						<ResourceListRow
+							index={context.book_id}
+							content={(
+								<BookListRow data={context} />
+							)}
+						/>
+					)}
+				/>
+			</PageContent>
+		</Page>
+	);
 }
---
+++ src/web/Pages/Books/BookListRow.js
@@ -0,0 +1,4 @@
+export default function BookListRow()
+{
+    return "Row";
+}
